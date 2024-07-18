1f5e8be593ce2ae29f8909ce6da8aa675a16696e
matijs <matijs@gmail.com>
2024-07-18 21:53:43 +0200

    test: add temp

---
+++ temp.tf
@@ -0,0 +1,42 @@
+resource "github_repository" "temp" {
+  name                   = "temp"
+  allow_auto_merge       = true
+  allow_merge_commit     = false
+  delete_branch_on_merge = true
+
+  template {
+    owner      = "nl-design-system"
+    repository = "example"
+  }
+}
+
+resource "github_repository_ruleset" "temp-main" {
+  name        = "main"
+  repository  = github_repository.temp.name
+  target      = "branch"
+  enforcement = "active"
+
+  conditions {
+    ref_name {
+      include = ["main"]
+      exclude = []
+    }
+  }
+
+  rules {
+    required_linear_history = true
+
+    pull_request {
+      dismiss_stale_reviews_on_push     = true
+      required_approving_review_count   = 1
+      required_review_thread_resolution = true
+    }
+
+    required_status_checks {
+      required_check {
+        context = "test"
+      }
+      strict_required_status_checks_policy = true
+    }
+  }
+}
